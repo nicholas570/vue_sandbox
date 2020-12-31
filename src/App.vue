60d09bacab5226416e98e4469c1477ac42d08c8a
nicolas <nicolas.mercier570@gmail.com>
2020-12-31 10:26:19 +0100

    created a super hero list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HeroList :heroes="heroes" @add-hero="updateHeroes" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import HeroList from "./components/HeroList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    HeroList
+  },
+  data() {
+    return {
+      heroes: [
+        { id: 1, name: "Bruce Wayne" },
+        { id: 2, name: "Victor Stone" },
+        { id: 3, name: "Diana" },
+        { id: 4, name: "Lois Lane" }
+      ]
+    };
+  },
+  methods: {
+    updateHeroes(hero) {
+      this.heroes = [
+        ...this.heroes,
+        { id: this.heroes.length + 1, name: hero }
+      ];
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
