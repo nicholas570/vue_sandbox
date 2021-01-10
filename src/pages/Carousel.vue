f84d10f36e23529e75d3f3251bad78ce417fff87
nicolas <nicolas.mercier570@gmail.com>
2021-01-10 17:17:29 +0100

    carousel done

--- src/pages/Carousel.vue
+++ src/pages/Carousel.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="flex flex-wrap w-full">
-    <div class="absolute w-full bg-blue-600" style="height:350px">helo</div>
-    <div class="absolute w-full bg-yellow-600" style="height:350px">helo</div>
-    <div class="absolute w-full bg-green-600" style="height:350px">helo</div>
+  <div class="flex flex-wrap w-full relative">
+    <div class="absolute w-full" v-for="(color, index) in sliders" :key="color">
+      <transition name="fade">
+        <div
+          v-if="currentSlide == index"
+          :class="color"
+          style="height:350px"
+        ></div>
+      </transition>
+    </div>
+    <div class="w-full" style="height:340px">
+      <div class="absolute bottom-0 w-full flex justify-center">
+        <div
+          v-for="(slider, index) in sliders"
+          :key="slider"
+          @click="makeActive(index)"
+          :class="currentSlide == index ? 'bg-gray-700' : 'bg-gray-300'"
+          class="w-4 h-4 mx-2 rounded-full cursor-pointer shadow-md"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      currentSlide: 1,
+      sliders: ["bg-green-600", "bg-blue-600", "bg-yellow-600"],
+      interval: ""
+    };
+  },
+  methods: {
+    makeActive(index) {
+      this.currentSlide = index;
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.currentSlide = this.currentSlide === 2 ? 0 : this.currentSlide + 1;
+    }, 2500);
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  }
+};
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+</style>
